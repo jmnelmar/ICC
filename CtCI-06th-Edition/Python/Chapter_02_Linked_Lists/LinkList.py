7272cf0b528740afa26a8f464bf705f6543a4786
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-04-24 23:03:22 -0700

    adding fix's to linklist class

--- CtCI-06th-Edition/Python/Chapter_02_Linked_Lists/LinkList.py
+++ CtCI-06th-Edition/Python/Chapter_02_Linked_Lists/LinkList.py
@@ -9,21 +9,36 @@ class LinkList:
         self.head = Node(value)
 
     def add(self, value):
-        while self.head.next is not None:
-             self.head = self.head.next
-        self.head.next = Node(value)
+        aux = self.head
+        if aux is None:
+            aux = Node(value)
+            return
+
+        while aux.next is not None:
+             aux = aux.next
+        aux.next = Node(value)
 
     def __str__(self):
         s = ""
-        while head is not None:
-            s += f"({head.vale}) > "
+        aux = self.head 
+        while aux is not None:
+            s += f"({aux.value}) > "
+            aux = aux.next
         s += "None"
+        return s
     
     def __len__(self):
         count = 0
-        while head is not None:
+        aux= self.head
+        while aux is not None:
             count += 1
+            aux = aux.next
         return count
 
 link_list = LinkList(5)
-link_list.__append__
+link_list.add(4)
+link_list.add(3)
+link_list.add(2)
+link_list.add(1)
+
+print(str(link_list))
