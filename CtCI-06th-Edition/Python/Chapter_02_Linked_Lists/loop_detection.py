bc39a2105d9530395373571529d177edf9bfcc65
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-04-29 18:37:21 -0700

    adding unit tests

--- CtCI-06th-Edition/Python/Chapter_02_Linked_Lists/loop_detection.py
+++ CtCI-06th-Edition/Python/Chapter_02_Linked_Lists/loop_detection.py
@@ -11,15 +11,16 @@
     C
     Hints: #50, #69, #83, #90
 """
+import pytest
 from LinkList import LinkList, Node
 def loop_detection(lst:Node):
     map = {}
     while lst is not None:
-        if lst.value not in map:
-            map[lst.value] = 1
+        if lst not in map:
+            map[lst] = 1
         else:
-            map[lst.value] += 1
-        if map[lst.value] > 1:
+            map[lst] += 1
+        if map[lst] > 1:
             break
         lst = lst.next
     return lst
@@ -28,5 +29,27 @@ lst = LinkList(values=['A','B','C','D','E'])
 lst.head.next.next.next.next.next = lst.head.next.next
 print(loop_detection(lst.head).value)
 
+@pytest.mark.parametrize(
+    "value1, value2, value3, expected",
+    [
+        ( ['A','B','C','D','E'], 2,'C', True ),
+        ( ['A','B','C','D','E'], 2,'F', False ),
+        ( ['A','B','C','C','D','E'], 1,'B', True ),
+        ( ['A','B','C','C','D','E'], 0,'B', False ),
+        ( ['A','B','C','C','D','E'], 1,'B', True )
+    ]
+)
+def test_loop_detection(value1, value2, value3, expected):
+    lst = LinkList(values=value1)
+    root = lst.head
+    tail = lst.head
+    for _ in range(value2):
+        root = root.next
+
+    while tail.next is not None:
+        tail = tail.next
+    tail.next = root
+
+    assert (loop_detection(lst.head).value == value3) == expected
 
 #a b c d e c
