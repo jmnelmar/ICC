4ffd34bc6a236e7a278c96b1803c72311c32eee8
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-06-02 16:42:03 -0700

    .

---
+++ CtCI-06th-Edition/Python/leetcode_challengues/Easy/20_valid_parentheses.py
@@ -0,0 +1,75 @@
+import pytest
+from collections import deque
+def isValid(s: str) -> bool:
+    stack= []
+    map = {")":"(","]":"[", "}":"{" }
+    for c in s:
+        if c in map:
+            top = stack.pop() if stack else "!"
+
+            if map[c] != top:
+                return False
+        else:
+            stack.append(c) 
+    return not stack
+
+def isValidII(s) -> bool:
+    stack = []
+    map = {")":"(","]":"[", "}":"{" }
+
+    for c in s:
+        if c in map:
+            top = stack.pop() if stack else "@"
+            if top != map[c]:
+                return False
+        else:
+            stack.append(c)
+    
+    return  len(stack) == 0
+
+
+
+@pytest.mark.parametrize(
+    's,expected',
+    [
+        ("()",True),
+        ("()[]{}",True),
+        ("(]",False),
+        ("{[]}",True),
+        ("]",False),
+        ("){",False),
+        ("([({{[()]}})])",True),
+        ("([({{[(]}})])",False),
+        ("(){}}{",False),
+    ]
+)
+def test_isvalid(s, expected):
+    assert isValidII(s) == expected
+
+
+
+s = "()"
+print(f"the string:{s} is {isValid(s)}")
+s = "()[]{}"
+print(f"the string:{s} is {isValid(s)}")
+s = "(]"
+print(f"the string:{s} is {isValid(s)}")
+s ="{[]}"
+print(f"the string:{s} is {isValid(s)}")
+s ="]"
+print(f"the string:{s} is {isValid(s)}")
+s ="){"
+print(f"the string:{s} is {isValid(s)}")
+s ="([({{[()]}})])"
+print(f"the string:{s} is {isValid(s)}")
+s ="([({{[(]}})])"
+print(f"the string:{s} is {isValid(s)}")
+s="(){}}{"
+print(f"the string:{s} is {isValid(s)}")
+s="(])"
+print(f"the string:{s} is {isValid(s)}")
+#q = deque()
+#q.append("(")
+#q.append(")")
+#print(q)
+#print(q[-1])
