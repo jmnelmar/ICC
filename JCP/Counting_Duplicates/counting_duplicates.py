c85097426adebfdbb158f5deedc0274296bbc420
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-08-09 20:50:38 -0700

    adding counting duplicates usign dictionaries in python

--- JCP/Counting_Duplicates/counting_duplicates.py
+++ JCP/Counting_Duplicates/counting_duplicates.py
@@ -3,4 +3,14 @@ def counting_duplicates(word):
     result = Counter(word)
     print(result)
 
+def counting_duplicates_II(word):
+    result = dict()
+    for c in word:
+        if c in result:
+            result[c] += 1
+        else:
+            result[c] = 1
+    print(result)
+
 counting_duplicates("anona")
+counting_duplicates_II("anona")
