212d9bb323d678847f0c7efd214e55ea8c38d63e
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-08-09 20:10:06 -0700

    adding JCP folder

---
+++ JCP/README.md
@@ -0,0 +1,4 @@
+![Java Rocks!](resources/java.svg)
+# Java Coding Problems
+
+## This is a compendium of the problems in the book Java Coding problems by Anghel Leonard
---
+++ Python/EOPIP/QuaternarianSearch.py
@@ -0,0 +1,13 @@
+mylist = [[1,1,0,1],[1,0,0,0],[1,0,0,1],[0,1,1,0]]
+
+print(mylist[3][0])
+
+
+def quaternion_search(pieces):
+    height = 4
+    width = 4
+
+    
+
+    
+
