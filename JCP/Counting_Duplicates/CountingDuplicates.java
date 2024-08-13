da84ea579aa84d7361168224e2617c6d1c826c3e
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-08-12 18:20:50 -0700

    Adding unicode counting chars in java

--- JCP/Counting_Duplicates/CountingDuplicates.java
+++ JCP/Counting_Duplicates/CountingDuplicates.java
@@ -50,4 +50,29 @@ public class CountingDuplicates{
                                         collect(Collectors.groupingBy(c->c,Collectors.counting()));
         return result;
     }
+
+    /*  Counting duplicates includin unicode characters
+    *   to include unicode characters are composed for two values a hig value and a low values this is known 
+    *   as surrogate pair, when we are dealing with unicode we only need one of the pair and skip the other
+    *   value in teh pair.
+    *   to identify if the character is an unicode is using the codePintAt function to get the numeric value
+    *   of the character, next get the value of the chars and finally using charCount if this return 2 values
+    *   we have a unicode in this char with the pair in the string, get the first one and skip the second one
+    *   by increasing the counter.
+    */
+
+    public static Map<String, Integer> countDuplicatesIII(String word){
+        Map<String,Integer> result = new HashMap<>();
+
+        for(int i = 0; i < word.length(); i++){
+            int cp = word.codePointAt(i);
+            String ch = String.valueOf(Character.toChars(cp));
+            if(Character.charCount(cp) == 2){// 2 means surrogate pair
+                i++;//skping the next value.
+            }
+            result.compute(ch, (k,v) -> (v == null)?1:++v);
+        }
+
+        return result;
+    }
 }
