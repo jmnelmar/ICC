81e1446fea5b79d05d6cce09b375c451550db47b
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-08-14 21:13:57 -0700

    adding remove duplicates in python

--- LeetCode/TopInterviewQuestions/Arrays/removeduplicates.py
+++ LeetCode/TopInterviewQuestions/Arrays/removeduplicates.py
@@ -1,3 +1,12 @@
 '''
 Remove duplicates from a sorted array
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
 '''
+def removeDuplicates(nums) -> int:
+    i = 0
+    while i > len(nums) - 1:
+        if nums[i] == nums[i + 1]:
+            del(nums[i])
+        else:
+            i += 1
+    return len(nums)
