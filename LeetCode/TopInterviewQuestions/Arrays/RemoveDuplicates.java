eececddcf4aa5cf89a0f8ff06a044b7615ba736d
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-08-14 21:20:46 -0700

    removing duplicates

--- LeetCode/TopInterviewQuestions/Arrays/RemoveDuplicates.java
+++ LeetCode/TopInterviewQuestions/Arrays/RemoveDuplicates.java
@@ -1,9 +1,21 @@
-package ICC.ICC.LeetCode.TopInterviewQuestions;
+package ICC.ICC.LeetCode.TopInterviewQuestions.Arrays;
 /*
  * Remove duplicates from a sorted array
 https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
 
  */
 public class RemoveDuplicates {
-    
+    public static int removeDuplicates(int[] nums){
+        if(nums.length == 0)
+            return 0;
+        
+        int j = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[j]){
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
 }
