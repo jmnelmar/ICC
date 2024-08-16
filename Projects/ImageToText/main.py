2100a2c2c7e06de58040a7d0bd61d94d2e1375ab
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-08-15 21:58:27 -0700

    .

--- Projects/ImageToText/main.py
+++ Projects/ImageToText/main.py
@@ -2,6 +2,14 @@ import pytesseract
 import PIL.Image
 import cv2
 
-myconfig = r"--psm 6 --oem 3"
+myconfig = r"--psm 11 --oem 3"
 text = pytesseract.image_to_string(PIL.Image.open("example.png"), config=myconfig)
 print(text)
+
+img = cv2.imread("naruto2.jpg")
+height, width, _ = img.shape
+
+boxes = pytesseract.image_to_boxes(img, config=myconfig)
+for box in boxes.splitlines():
+    box = box.split(" ")
+    img = cv2.rectangle(img,(int(box[1], height - int(box[2], int(box[3], int(box[4]))))))
