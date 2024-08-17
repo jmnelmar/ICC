e5d674d560e4bc3372dc1ca808733bcfbae1b5d4
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-08-17 00:30:45 -0700

    adding maxprofit o(n) solution

--- LeetCode/TopInterviewQuestions/Arrays/best_time_to_sell_buy_stock.py
+++ LeetCode/TopInterviewQuestions/Arrays/best_time_to_sell_buy_stock.py
@@ -12,15 +12,18 @@ def maxProfit( prices:list) -> int:
 
 #Eficient solution.
 def maxProfit_(prices:list) -> int:
-    max_stock,min_stock,max_profit, profit = 0,0,0,0
+    min_price, max_profit = float("inf"), 0
 
     for i in range(len(prices)):
-        max_stock = max( prices[1:] )
-        if max_stock > prices[i]:
-            
+        if prices[i] < min_price:
+            min_price = prices[i]
+        max_profit = max(max_profit, (prices[i] - min_price))
+
+    return max_profit    
+
     
 
 prices = [7,1,5,3,6,4]
-print(prices[1:])
-#print(maxProfit(prices))
+#print(prices[1:])
+print(maxProfit_(prices))
     
