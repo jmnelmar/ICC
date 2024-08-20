b3b7f283930258841f64701a99dbaaca64bcd267
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-08-20 09:41:20 -0700

    adding contain duplicates in python

--- LeetCode/TopInterviewQuestions/Arrays/Python/contain_duplicates.py
+++ LeetCode/TopInterviewQuestions/Arrays/Python/contain_duplicates.py
@@ -1,2 +1,13 @@
-def containsDuplicate(nums:list[int]):
-    pass
+from collections import Counter
+def contains_duplicate(nums:list[int]) -> bool:
+    map_nums = Counter(nums)
+
+    for n in map_nums.keys():
+        if map_nums[n] > 1:
+            return True
+    return False
+
+a = [1,2,3,4,5,5,6,7]
+print(contains_duplicate(a))
+b = [1,2,3,4,5,6,7]
+print(contains_duplicate(b))
