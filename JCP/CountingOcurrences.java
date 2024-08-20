7eb45ab8d5e375bf3d307b15ecb50435d9e845ea
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-08-19 21:39:11 -0700

    adding unicode support to  a character in java

--- JCP/CountingOcurrences.java
+++ JCP/CountingOcurrences.java
@@ -6,6 +6,9 @@ public class CountingOcurrences {
     }
     
     public static int countOcurrencesOfACharacter(String str, String ch){
-        return str.length() - str.replace(ch, "").length();
+        if(ch.codePointCount(0, ch.length()) > 1)
+            return -1;
+        int result =  str.length() - str.replace(ch, "").length();
+        return ch.length() == 2?result/2:result;
     }
 }
