3347420b0a044fd4f669471e874a4ee278487419
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-08-28 22:53:38 -0700

    adding anagrams

---
+++ linkedin/top_20_most_asked/CheckAnagram.java
@@ -0,0 +1,23 @@
+package ICC.linkedin.top_20_most_asked;
+
+import java.util.Arrays;
+
+public class CheckAnagram {
+
+    public static boolean IsAnagram(String str1, String str2){
+
+        char[] aux1 = str1.toCharArray();
+        char[] aux2 = str2.toCharArray();
+
+        Arrays.sort(aux1);
+        Arrays.sort(aux2);
+
+        return Arrays.equals(aux1, aux2);
+    }
+
+    public static void main(String[] args){
+        String str1 = "listen", str2="silent";
+        System.out.println(IsAnagram(str1, str2));
+    }
+    
+}
