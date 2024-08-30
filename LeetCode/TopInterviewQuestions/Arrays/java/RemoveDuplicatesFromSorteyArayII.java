58cb79ae38f86e6d3587f194e0d2ac8b0062a78a
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-08-29 21:15:12 -0700

    remove duplicates II optimized methos

--- LeetCode/TopInterviewQuestions/Arrays/java/RemoveDuplicatesFromSorteyArayII.java
+++ LeetCode/TopInterviewQuestions/Arrays/java/RemoveDuplicatesFromSorteyArayII.java
@@ -17,6 +17,8 @@ public class RemoveDuplicatesFromSorteyArayII {
     public static int removeDuplicates(int[] nums){
 
         int j = 0;
+        if( nums.length < 3)
+            return nums.length;
 
         for(int i = 0; i < nums.length; i++){
             if( nums[j] != nums[i] ){
@@ -32,20 +34,60 @@ public class RemoveDuplicatesFromSorteyArayII {
             }
 
         }
+        if( j == 0 )
+            return 2;
         return j + 1;
+    }
+
+    public static int removeDuplicatesII(int[] nums){
+        int cont = 1, i = 1;
+        for( int j = 1; j < nums.length ; j++ ){
+            if( nums[j] == nums[j - 1] )
+                cont++;
+            else
+                cont = 1;
+
+            if( cont <= 2 ){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
     }
 
     public static void main(String[] args){
         int[] nums2 = {1,1,1,2,2,3};
-        String result =removeDuplicates(nums2) + " ";
+        String result =removeDuplicatesII(nums2) + " ";
         result += Arrays.toString(nums2);
 
         System.out.println(result);
 
         int[] nums = {0,0,1,1,1,1,2,3,3};
-        result =removeDuplicates(nums) + " ";
+        result =removeDuplicatesII(nums) + " ";
         result += Arrays.toString(nums);
         System.out.println(result);
+
+        int[] nums3 = {1,1};
+        result = removeDuplicatesII(nums3) + " ";
+        result += Arrays.toString(nums3);
+        System.out.println(result);
+
+        int[] nums4 = {1,1,1};
+        result = removeDuplicatesII(nums4) + " ";
+        result += Arrays.toString(nums4);
+        System.out.println(result);
+
+        int[] nums5 = {1,1,2};
+        result = removeDuplicatesII(nums5) + " ";
+        result += Arrays.toString(nums5);
+        System.out.println(result);
+
+        int[] nums6 = {1,1,2,2};
+        result = removeDuplicatesII(nums6) + " ";
+        result += Arrays.toString(nums6);
+        System.out.println(result);
+
     }
     
 }
