816345624e9e4dba12356e32c1dcacac90638f05
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-09-02 10:20:30 -0700

    adding some functionality

---
+++ LeetCode/medium_challengues/java/LongestPalindromicSubStr.java
@@ -0,0 +1,18 @@
+package ICC.LeetCode.medium_challengues.java;
+
+public class LongestPalindromicSubStr {
+    public String longestPalindrome(String s) {
+
+        for(int i = 0; i < s.length(); i++){
+            StringBuilder build = new StringBuilder(s.substring(0, s.length()- 1 - i));
+            
+        }
+        return "";
+    }
+
+    public static void main(String[] args){
+        StringBuilder builder = new StringBuilder("Hola");
+        
+    }
+
+}
