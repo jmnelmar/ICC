05bb4a9a2974d5e47af3ac0ae8e02802b8259246
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-09-05 21:10:21 -0700

    adding comments to minimun move

--- LeetCode/facebook_aka_meta/DeletingNodesFromAnArray.py
+++ LeetCode/facebook_aka_meta/DeletingNodesFromAnArray.py
@@ -1,1 +1,16 @@
-def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+def modifiedList( nums, head):
+    while head and head.value in nums:
+        head = head.next
+
+    if not head:
+        return None
+    
+    current = head
+
+    while current.next:
+        if current.next.value in nums:
+            current.next = current.next.next
+        else:
+            current = current.next
+
+    return head
--- LeetCode/facebook_aka_meta/MinRemoveToMakeValid.py
+++ LeetCode/facebook_aka_meta/MinRemoveToMakeValid.py
@@ -1,22 +1,31 @@
 import pytest
 
 def mminRemoveToMakeValid(s:str) -> str:
-    indexes_to_remove = set()
+    index_to_remove = set()
     stack = []
-    for i,c in enumerate(s):
-        if c not in "()":
+    for i, c in enumerate(s):
+        if not c in "()":
             continue
         
-        if c == "()":
+        if c == "(": #adding open parenthesis to the stack
             stack.append(i)
-        elif not stack:
-            indexes_to_remove.add(i)
-        else:
+        elif not stack: #if is closing and there no openning parenthesis in the stack add to indexes to remove
+            index_to_remove.add(i)
+        else: # if the character is closing removing from the stack
             stack.pop()
-    indexes_to_remove = indexes_to_remove.union(set(stack))
-    string_builder = []
+
+    index_to_remove = index_to_remove.union(set(stack)) #in case that were any opening tag without closing tag lets add it to the indexes to remove
+    string_builder = [] #creating the variable which will hold the string to return.
+
     for i,c in enumerate(s):
-        if i not in indexes_to_remove:
+        if i not in index_to_remove:
             string_builder.append(c)
+
     return "".join(string_builder)
+
+
+s = "lee(t(c)o)de)"
+print(mminRemoveToMakeValid(s))
+
+
 
