ec47169dc8fdf7bf4599e750fdf47b928acc0dc9
Nelson Jimenez <jmnel_mar@hotmail.com>
2024-09-05 21:24:49 -0700

    adding descriptions to deliting nodes from an array

--- LeetCode/facebook_aka_meta/DeletingNodesFromAnArray.py
+++ LeetCode/facebook_aka_meta/DeletingNodesFromAnArray.py
@@ -1,16 +1,20 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 def modifiedList( nums, head):
-    while head and head.value in nums:
+    values_to_remove = set(nums)
+    while head and head.val in values_to_remove: #handlig duplicates
         head = head.next
+        if not head: #in case head were null then return null
+            return None
 
-    if not head:
-        return None
-    
-    current = head
+        current = head #making a shadow copy to tleft head in the first node
+        while current.next:
+            if current.next.val in values_to_remove:
+                current.next = current.next.next
+            else:
+                current = current.next
+        return head
 
-    while current.next:
-        if current.next.value in nums:
-            current.next = current.next.next
-        else:
-            current = current.next
-
-    return head
